7abd3672d9dd14a444fbf7cc2c4e8d93df83942f
= <=>
2024-01-04 23:25:36 +0530

    Added AppLayout component

---
+++ src/components/layouts/appLayout.tsx
@@ -0,0 +1,7 @@
+export const AppLayout = () => {
+  return (
+    <div className="flex items-end justify-center text-center sm:block sm:px-4 sm:pt-4 sm:pb-32 bg-gradient-to-tr from-indigo-800 to-indigo-500 w-full h-screen">
+      <div className="inline-block w-fit overflow-hidden transition-all transform bg-white sm:m-16 sm:align-middle sm:max-w-xl"></div>
+    </div>
+  );
+};
